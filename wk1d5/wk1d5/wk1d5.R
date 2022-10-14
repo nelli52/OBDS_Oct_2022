59c9d64c74ad9a672a52a02cc076f963267aaa1a
nelliott <natalina.elliott@paediatrics.ox.ac.uk>
2022-10-14 16:17:56 +0100

    adding files

---
+++ wk1d5/wk1d5/wk1d5.R
@@ -0,0 +1,12 @@
+# in Terminal
+    # cd /t1-data/project/obds/nelliott/2_baseR/projects
+    # mkdir wk1d5
+    # import tidyvere excercise
+        # cp /project/obds/albrecht/resources/templates/tidyverse.Rmd .
+
+# In console
+setwd (dir = "/project/obds/nelliott/2_baseR/projects/wk1d5")
+
+# in packages make sure tidyverse is clicked to load the package
+
+
